9ee4f37ad903bbda790fea9f6b118f8ed0138c46
Prayash Thapa <prayasht@gmail.com>
2017-08-12 17:07:05 -0600

    Deploy

--- v4/.cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/effulgence/dev/effulgence.io/v4/src/layouts/index.js"
-  import data from "/Users/effulgence/dev/effulgence.io/v4/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
